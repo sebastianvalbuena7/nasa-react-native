28ac51141fe309f3d389d089ac39b62531655d1d
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-07-08 13:49:18 -0500

    Implementando Imagen

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,2 +1,19 @@
 const API_KEY = '5XqhZ7s5dV9SOyZXcVJYhQY4P4j9TG7LAQdhUBRn'
 const API_URL = 'https://api.nasa.gov/planetary/apod'
+
+export default async (urlParams?: string) => {
+    try {
+        const response = await fetch(
+            `${API_URL}?api_key=${API_KEY}${
+                typeof urlParams !== 'undefined' && 
+                urlParams?.length > 0 
+                ? urlParams : ''}`
+                )
+
+        const data = response.json()
+
+        return Promise.resolve(data)
+    } catch (error) {
+        return Promise.reject(error)
+    }
+}
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
 import { View, StyleSheet } from "react-native"
 import Header from "../../components/Header/Header"
+import TodaysImage from '../../components/TodaysImage'
+import fetchApi from "../../utils/fetch"
+import { PostImage } from '../../types'
 
 const Home = () => {
+    const [todaysImage, setTodaysImage] = useState<PostImage>({})
+
+    useEffect(() => {
+        const loadTodaysImage = async () => {
+            try {
+                const todaysImageResponse = await fetchApi()
+                setTodaysImage(todaysImageResponse)
+            } catch (error) {
+                console.error(error)
+                setTodaysImage({})
+            }
+        }
+
+        loadTodaysImage().catch(null)
+    }, [])
+
     return (
         <View style={styles.container}>
             <Header />
+            <TodaysImage {...todaysImage} />
         </View>
     )
 }
